a8ab0284ef2ae5740421baa925828aac37530e0a
K CHaitu <kchaithanya.2010@gmail.com>
2021-11-18 15:27:33 +0530

    updated

--- jenkinsfile_deploy.groovy
+++ jenkinsfile_deploy.groovy
@@ -1,10 +1,17 @@
  //declarative pipeline
 pipeline{
     agent any
+    parameters{
+    string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'from which branch artifacts want to deploy',) 
+    string(name: 'BUILD_NUM', defaultValue: 'master', description: 'from which BUILD NUMBER artifacts want to deploy',)
+    }
     stages{
         stage("download artifacts"){
             steps{
                 println "downloading artifacts from s3"
+                sh "aws s3 ls"
+                sh "aws s3 ls s3://chaituart"
+                sh "aws s3 ls s3://chaituart/${BRANCH_NAME}/${BUILD_NUM}"
             }
         }
         stage("copy artifacts"){
