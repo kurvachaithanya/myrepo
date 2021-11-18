3da741b0e96437f76c20518789671f08543dc29c
K CHaitu <kchaithanya.2010@gmail.com>
2021-11-18 15:41:47 +0530

    updated

--- jenkinsfile_deploy.groovy
+++ jenkinsfile_deploy.groovy
@@ -3,7 +3,8 @@ pipeline{
     agent any
     parameters{
     string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'from which branch artifacts want to deploy',) 
-    string(name: 'BUILD_NUM', defaultValue: 'master', description: 'from which BUILD NUMBER artifacts want to deploy',)
+    string(name: 'BUILD_NUM', defaultValue: '', description: 'from which BUILD NUMBER artifacts want to deploy',)
+    string(name: 'SERVER_IP', defaultValue: '', description: 'To which server artifacts want to deploy',)
     }
     stages{
         stage("download artifacts"){
@@ -12,11 +13,13 @@ pipeline{
                 sh "aws s3 ls"
                 sh "aws s3 ls s3://chaituart"
                 sh "aws s3 ls s3://chaituart/${BRANCH_NAME}/${BUILD_NUM}"
+                sh "aws s3 cp s3://chaituart/${BRANCH_NAME}/${BUILD_NUM}/hello-${BUILD_NUM}.war ."
             }
         }
         stage("copy artifacts"){
             steps{
                 println "copy artifacts to tomcat server"
+                sh "ssh -i /tmp/mine.pem ec2@user:${SERVER_IP}"systemctl status tomcat"
             }
         }
     }
